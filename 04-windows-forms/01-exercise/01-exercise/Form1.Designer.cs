9126008a02398a3bcf9c3b1230ebafa0a637d4a2
SrMazas <info.otero.gabriel@gmail.com>
2022-10-07 13:57:58 +0200

    Save

--- 04-windows-forms/01-exercise/01-exercise/Form1.Designer.cs
+++ 04-windows-forms/01-exercise/01-exercise/Form1.Designer.cs
@@ -34,27 +34,27 @@
             // 
             // button1
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.button1.Location = new System.Drawing.Point(12, 9);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(374, 92);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             // 
             // button2
             // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.button2.Location = new System.Drawing.Point(414, 9);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(374, 92);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             // 
             // Form1
             // 
@@ -65,6 +65,8 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.ResumeLayout(false);
 
         }
--- 04-windows-forms/01-exercise/01-exercise/Form1.cs
+++ 04-windows-forms/01-exercise/01-exercise/Form1.cs
@@ -6,5 +6,35 @@ namespace _01_exercise
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text="Mouse Tester";   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x= e.Location.X;
+            int y= e.Location.Y;
+
+            if (sender is Button)
+            {
+                x += ((Button)sender).Location.X;
+                y += ((Button)sender).Location.Y;
+            }
+            
+            this.Text = $"X:{x} | Y:{y}";
+        }
+
     }
 }
