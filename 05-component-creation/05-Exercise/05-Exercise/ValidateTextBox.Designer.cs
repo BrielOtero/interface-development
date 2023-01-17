75964f4695c62c1c71937cc7687a579f2b28b263
Gabriel O <info.otero.gabriel@gmail.com>
2023-01-17 21:52:24 +0100

    save

--- 05-component-creation/05-Exercise/05-Exercise/ValidateTextBox.Designer.cs
+++ 05-component-creation/05-Exercise/05-Exercise/ValidateTextBox.Designer.cs
@@ -28,11 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(10, 10);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(100, 20);
+            this.txtText.TabIndex = 0;
+            this.txtText.TextChanged += new System.EventHandler(this.txtText_TextChanged);
+            // 
+            // ValidateTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtText);
+            this.Name = "ValidateTextBox";
+            this.Size = new System.Drawing.Size(121, 40);
+            this.SizeChanged += new System.EventHandler(this.ValidateTextBox_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtText;
     }
 }
--- 05-component-creation/05-Exercise/05-Exercise/ValidateTextBox.cs
+++ 05-component-creation/05-Exercise/05-Exercise/ValidateTextBox.cs
@@ -7,14 +7,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace _05_Exercise
 {
-    public partial class ValidateTextBox: UserControl
+    public partial class ValidateTextBox : UserControl
     {
+        private eType type;
+        private bool isValid = false;
+       
         public ValidateTextBox()
         {
             InitializeComponent();
+            asignSize();
+        }
+
+        private void ValidateTextBox_SizeChanged(object sender, EventArgs e)
+        {
+            asignSize();
+        }
+
+        private void asignSize()
+        {
+            this.Height = txtText.Height + 20;
+            txtText.Width = this.Width - 20;
+        }
+
+
+        [Category("Property")]
+        [Description("Value of " + nameof(Text))]
+        public string Text
+        {
+            set { txtText.Text = value; }
+            get { return txtText.Text; }
+        }
+
+        [Category("Property")]
+        [Description("Value of " + nameof(Multiline))]
+        public bool Multiline
+        {
+            set { txtText.Multiline = value; }
+            get { return txtText.Multiline; }
+        }
+
+
+
+        [Category("Property")]
+        [Description("Value of " + nameof(Type))]
+        public eType Type
+        {
+            set
+            {
+                type = value;
+                checkText();
+            }
+            get { return type; }
+        }
+
+        [Category("The Property change")]
+        [Description("Fired when the text property change")]
+        public event EventHandler TextChanged;
+
+       protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+            Color penColor = isValid ? Color.Green : Color.Red;
+            int thickness = 20;
+            Pen pen = new Pen(penColor, thickness);
+
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.DrawRectangle(pen, 5, 5, this.Width - 5, this.Height - 5);
+        }
+
+        private void txtText_TextChanged(object sender, EventArgs e)
+        {
+            TextChanged?.Invoke(this, EventArgs.Empty);
+            checkText();
+        }
+
+        private void checkText()
+        {
+            string text = txtText.Text;
+
+            switch (Type)
+            {
+                case eType.NUMERIC:
+                    text = text.Trim();
+                    isValid = int.TryParse(text, out int n);
+                    break;
+
+                case eType.TEXTUAL:
+                    isValid = text.All(x => Char.IsLetter(x) || x == ' ');
+                    break;
+            }
+            this.Refresh();
         }
     }
 }
